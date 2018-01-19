110dc34c6bdbd25ee4bf2b5d69aeebf5c0a00c8a
Marcin Chiliński <chilo696@gmail.com>
2018-01-19 17:04:49 +0100

    App

---
+++ src/components/add_box.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class AddBox extends Component {
+  render() {
+    return (
+      <button type="submit"/>
+    );
+    }
+  }
+  
+  export default AddBox;
